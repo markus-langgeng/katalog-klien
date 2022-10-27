8e6a1dff252a037919b4dd7d1db334bb1b04c34f
Marcel <marcelano21212@gmail.com>
2022-10-27 14:32:33 +0700

    add login

--- src/app/views/login/index.php
+++ src/app/views/login/index.php
@@ -1,3 +1,34 @@
-   <h1>HALAMAN LOGINNN</h1>
-   <p>Buat front end nya di src/app/views/login/index.php</p>
-   <p> <?= $data["nama"]; ?> </p>
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>NONE</title>
+    </head>
+    <body>
+        <nav>
+    <div class="container">
+        <div class="forms">
+            <div class="form login">
+                <span class="title">Login</span>
+
+                <form action="#">
+                    <div class="input-field">
+                        <input type="text" placeholder="Enter your username" required>
+                    </div>
+                    <div class="input-field">
+                        <input type="password" class="password" placeholder="Enter your password" required>
+                        <i class="uil uil-lock icon"></i>
+                    </div>
+
+                    <div class="input-field button">
+                        <input type="button" value="Login">
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</nav>
+</body>
+</html>
